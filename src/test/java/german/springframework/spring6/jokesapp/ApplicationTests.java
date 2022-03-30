569391c59a8671ba24efed973cc4761e9ec4e08d
GermanAcevedo <germav@gmail.com>
2022-03-30 11:13:19 -0400

    Spring boot project created close #1

---
+++ src/test/java/german/springframework/spring6/jokesapp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package german.springframework.spring6.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
