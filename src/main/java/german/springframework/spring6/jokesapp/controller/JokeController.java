3569e30c448d70ecc13a6526b8561079d3f2cea1
GermanAcevedo <germav@gmail.com>
2022-03-30 13:39:00 -0400

    Controll implemented, close #4

---
+++ src/main/java/german/springframework/spring6/jokesapp/controller/JokeController.java
@@ -0,0 +1,22 @@
+package german.springframework.spring6.jokesapp.controller;
+
+import german.springframework.spring6.jokesapp.JokeService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    public JokeController(@Qualifier("jokeServiceImpl") JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+}
